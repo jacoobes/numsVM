fc38a554bb64c9a5cb255bc50ba31fd0e098aac5
jacoobes <jacobnguyend@gmail.com>
2021-12-10 00:54:00 -0600

    disassembler now uses references and not owned enums

--- src/bytecode/disassemble.rs
+++ src/bytecode/disassemble.rs
@@ -1,8 +1,8 @@
-use super::{
+use crate::bytecode::{
     chunk::Chunk,
-    op_code::OpCode,
+    op_code::OpCode::*,
+    op_code::OpCode
 };
-use crate::bytecode::op_code::OpCode::*;
 
 pub fn disassemble_chunk(chunk: &Chunk) {
     println!("{:=^40}", " test chunk ");
@@ -18,13 +18,13 @@ pub fn disassemble_instruction(chunk: &Chunk, offset: &mut usize) -> usize {
     let instruction = chunk.instructions.get(*offset).expect("expected an opcode");
 
     match instruction {
-        0x00 => simple_instruction(offset, HALT),
-        0x01 => const_instruction(chunk, offset, LOAD_CONST),
-        0x02 => simple_instruction(offset, NEGATE),
-        0x03 => simple_instruction(offset, ADD),
-        0x04 => simple_instruction(offset, SUB),
-        0x05 => simple_instruction(offset, MUL),
-        0x06 => simple_instruction(offset, DIV),
+        0x00 => simple_instruction(offset, &HALT),
+        0x01 => const_instruction(chunk, offset, &LOAD_CONST),
+        0x02 => simple_instruction(offset, &NEGATE),
+        0x03 => simple_instruction(offset, &ADD),
+        0x04 => simple_instruction(offset, &SUB),
+        0x05 => simple_instruction(offset, &MUL),
+        0x06 => simple_instruction(offset, &DIV),
         _ => {
             panic!("unknown opcode!")
 
@@ -32,19 +32,19 @@ pub fn disassemble_instruction(chunk: &Chunk, offset: &mut usize) -> usize {
     }
 }
 
-fn simple_instruction(offset: &mut usize, instruction: OpCode) -> usize {
+fn simple_instruction(offset: &mut usize, instruction: &OpCode) -> usize {
     println!(
         "{:#04x}{:>20}",
-        Chunk::op_to_u8(&instruction),
+        Chunk::op_to_u8(instruction),
         format!("{:?}", instruction)
     );
     *offset + 1
 }
-fn const_instruction(chunk: &Chunk, offset: &mut usize, instruction: OpCode) -> usize {
+fn const_instruction(chunk: &Chunk, offset: &mut usize, instruction: &OpCode) -> usize {
     let loc_of_const = chunk.instructions[*offset + 1] as usize;
     print!(
         "{:#04x}{:>20}",
-        Chunk::op_to_u8(&instruction),
+        Chunk::op_to_u8(instruction),
         format!("{:?}", instruction)
     );
     print!("{:>10}", &loc_of_const);
--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,6 @@
 pub mod bytecode;
 pub mod vm;
 mod tests;
-
 use bytecode::data::Type;
 
-fn main() {
-    
-
-}
+fn main() {}
